5f4234000aa097195b11a8c4834ff400244a65cc
BillAndyQ <billandyqm@gmail.com>
2024-04-22 16:21:26 -0500

    si

--- project/urls.php
+++ project/urls.php
@@ -5,4 +5,3 @@ path("/login", "sign.login");
 path("/forget-password", "sign.forgetPass","forget_pass");
 path("/register", "register.register","register");
 path("/boleta-salida", "boleta_salida.boletaSalida","boleta_salida");
-
